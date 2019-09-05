e77337b40ab729248386e67e12e6ccf3449a7b64
Marcus Bengtsson <marcus_bengtsson_91@hotmail.com>
2019-09-05 10:41:21 +0200

    Initial commit

---
+++ DutchPainters/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DutchPainters.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
